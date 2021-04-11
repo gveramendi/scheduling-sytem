2b5a7e0caa1ef4e5e3b3d9d080f2e63ebdbbc276
Gerson M. Veramendi <gersonv@goja.com>
2021-04-11 18:26:26 -0400

    first commit.

---
+++ src/main/java/com/example/schedulingsytem/SchedulingSytemApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulingsytem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingSytemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingSytemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/schedulingsytem/SchedulingSytemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulingsytem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingSytemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
